7d5b4233fdefae33de85ded9b3ad511ecaf97f89
linhn <linhn@10.15.142.47>
2022-11-02 16:17:24 -0700

    I just create the panel for the mainMenu

--- src/main/java/edu/pacific/comp55/starter/MainMenu.java
+++ src/main/java/edu/pacific/comp55/starter/MainMenu.java
@@ -1,5 +1,21 @@
 package edu.pacific.comp55.starter;
+import acm.program.*;
 
-public class MainMenu {
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+
+import acm.graphics.*;
+public class MainMenu extends GraphicsProgram{
+	public static final int WINDOW_WIDTH = 1920;
+	public static final int WINDOW_HEIGHT = 1080;
+	public void init() {
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	}
+	public void run() {
+		System.out.println("Hello");
+	}
+	public static void main (String[] args) {
+		new MainMenu().start();
+	}
 
 }
