9a0301e00fc02296200085498473e10f5ea6796d
Joao Pedro de Favari Engel <j_defavariengel@u.pacific.edu>
2022-11-02 15:58:53 -0700

    Enum created with all the toppings we have.

--- src/main/java/edu/pacific/comp55/starter/ToppingType.java
+++ src/main/java/edu/pacific/comp55/starter/ToppingType.java
@@ -1,5 +1,20 @@
 package edu.pacific.comp55.starter;
+import java.lang.String;
 
-public class ToppingType {
+public enum ToppingType {
+	BACON, CHEESE, EGG, CAN, PINEAPPLE, CLOCK, ROCK;
+	
+	 public String toString() {
+		switch(this) {
+			case BACON: return "bacon";
+			case CHEESE: return "cheese";
+			case EGG: return "egg";
+			case CAN: return "can";
+			case PINEAPPLE: return "pineapple";
+			case CLOCK: return "clock";
+			case ROCK: return "rock";
+		}
+		return "n/a";
+	}
 
 }
