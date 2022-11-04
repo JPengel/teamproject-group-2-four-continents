4a11639d9a66eeffe06b6024813f9e1c582e9dd2
Javier <j_covarrubias1@u.pacific.edu>
2022-11-03 20:02:09 -0700

    Added the functions to the Mode class

--- src/main/java/edu/pacific/comp55/starter/Mode.java
+++ src/main/java/edu/pacific/comp55/starter/Mode.java
@@ -31,4 +31,77 @@ public class Mode extends GraphicsProgram{
 		System.out.println("Hello");
 	}
 	
+	public void drawBoard() {
+		//TODO Calls all other draw functions.
+	}
+	
+	public void drawPauseButton() {
+		//TODO Inserts Image of PauseButton.
+	}
+	
+	public void drawScore() {
+		//TODO Draws the current score and highscore.
+	}
+	
+	public void pausePressed() {
+		//TODO Creates new instance of PauseMenu and makes inPause true.
+	}
+	
+	public Topping getObject(int x, int y) {
+		return null;
+		//TODO Returns whatever topping is at the x and y coordinate.
+	}
+	
+	public boolean cutObject(Topping a) {
+		return false;
+		//TODO Return true if topping.isCut() returns true.
+	}
+	
+	public void resetAll() {
+		//TODO returns all variables back to their original forms.
+	}
+	
+	public void generateObject() {
+		//TODO creates a new object.
+	}
+	
+	public void exceedHighScore() {
+		//TODO if scoreCounter is greater than or equal to highscore then 
+		// make scoreCounter the new high score.
+	}
+	
+	public void fallenOffScreen(Topping t) {
+		//TODO If topping fell off, then delete it from Array List
+	}
+	
+	public void deleteTopping(Topping t) {
+		//TODO Deletes Image of topping from screen.
+	}
+	
+	public boolean knifeSharpened() {
+		return false;
+		//TODO Returns true if knife is sharpened
+	}
+	
+	public boolean inLine(Topping t) {
+		return false;
+		//TODO Retrun true if Topping is in comboLine.
+	}
+	
+	public void stopAll() {
+		//TODO Stops timer.
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+	}
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		
+	}
+	
+	public static void main (String[] args) {
+		new Mode().start();
+	}
 }
