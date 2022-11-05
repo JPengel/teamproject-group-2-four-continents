41911f1ea189ad35517ed0bf9deff37e81c29952
Joao Pedro de Favari Engel <j_defavariengel@u.pacific.edu>
2022-11-05 14:01:30 -0700

    Added pause menu images

--- src/main/java/edu/pacific/comp55/starter/PauseMenu.java
+++ src/main/java/edu/pacific/comp55/starter/PauseMenu.java
@@ -5,19 +5,29 @@ import acm.graphics.*;
 class PauseMenu  extends GraphicsProgram {
 	public static final int PROGRAM_WIDTH = 1920; 
 	public static final int PROGRAM_HEIGHT = 1080;
-	//private GImage backGround= new GImage("pauseMenuImages/Background.png");
-	//private GImage exit = new GImage("pauseMenuImages/Exit.png",985,686);
-	//private GImage continuE = new GImage("pauseMenuImages/Continue.png",580,376);
-	//private GImage retry = new GImage("pauseMenuImages/Retry.png",580,686);
+	private GImage backGround= new GImage("src/main/resources/Background.png");
+	private GImage exit = new GImage("src/main/resources/Exit.png",985,686);
+	private GImage continuE = new GImage("src/main/resources/Continue.png",580,376);
+	private GImage retry = new GImage("src/main/resources/Retry.png",580,686);
 	private boolean returN;
-	TimerMode timerModePause = new TimerMode();
-	NoWasteMode noWasteModePause = new NoWasteMode();
+	TimerMode timerModePause;
+	NoWasteMode noWasteModePause;
+	
+	public PauseMenu() {
+		
+	}
+	public PauseMenu(TimerMode timerMode) {
+		timerModePause = new TimerMode();
+	}
+	public PauseMenu(NoWasteMode noWasteMode) {
+		noWasteModePause = new NoWasteMode();
+	}
 	public void drawMenu() {
-//		System.out.println("javier was here");
-		//add(backGround);
-		//add(exit);
-		//add(continuE);
-		//add(retry);
+		//System.out.println("javier was here");
+		add(backGround);
+		add(exit);
+		add(continuE);
+		add(retry);
 		
 	}
 	public void init() {
