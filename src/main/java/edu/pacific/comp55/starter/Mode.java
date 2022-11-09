769d22876a431db072e7f516c35e4bf054feb4e5
linhn <linhn@10.15.142.47>
2022-11-09 15:37:57 -0800

    I just added Koba code

--- src/main/java/edu/pacific/comp55/starter/Mode.java
+++ src/main/java/edu/pacific/comp55/starter/Mode.java
@@ -1,5 +1,7 @@
 package edu.pacific.comp55.starter;
 import acm.program.*;
+import acm.util.RandomGenerator;
+
 import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.util.*;
@@ -8,19 +10,19 @@ import acm.graphics.*;
 public class Mode extends GraphicsProgram{
 	public static final int WINDOWS_WIDTH = 1920;
 	public static final int WINDOWS_HEIGHT = 1080;
-	
+	private static RandomGenerator probability, toppingChooser, hazardChooser, upgradeChooser;
 	protected ArrayList<Topping> objList;
 	protected GImage pauseButton;
 	protected GLabel bothScores;
 	protected PauseMenu PMenu;
 	protected GameOver gameOver;
-	protected int baconSliced, cheeseSliced, eggSliced;
+	protected int baconSliced, cheeseSliced, eggSliced, scoreCounter, comboCounter = 1;
 	protected ArrayList <Topping> toppingArray = new ArrayList<Topping>();
 	protected Timer Timer;
-	protected int scoreCounter;
 	protected GLine comboLine;
-	protected int comboCounter = 1;
 	protected MainMenu MMenu;
+	private static boolean isTimerMode;
+	private static double comboEntryX, comboEntryY, comboLaterX, comboLaterY, lineSlope = 0, lineB = 0;
 	
 	public Mode(MainMenu m) {
 		MMenu = m;
@@ -64,7 +66,20 @@ public class Mode extends GraphicsProgram{
 	
 	public void generateObject() {
 		//TODO creates a new object.			
-		
+		int chance = probability.nextInt(1, 100);
+		if(chance < 81) { //Toppings 80% chance
+			objList.add(new Topping(ToppingType.values()[toppingChooser.nextInt(0,2)]));
+		} else if (chance > 80 && chance < 91) { //Hazards 10% chance
+			objList.add(new Topping(ToppingType.values()[hazardChooser.nextInt(3,4)]));
+		} else { //Upgrades 10% chance
+			if(isTimerMode) { 
+				objList.add(new Topping(ToppingType.values()[upgradeChooser.nextInt(5,6)]));
+			} else {
+				if(chance % 2 == 0) {
+					objList.add(new Topping(ToppingType.ROCK));
+				}
+			}
+		}
 	}
 	
 	public void exceedHighScore() {
