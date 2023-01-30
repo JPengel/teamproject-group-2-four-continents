a85d53f14e10830f181882e993c7f5be75d1720c
Joao Pedro de Favari Engel <j_defavariengel@u.pacific.edu>
2023-01-29 19:48:27 -0800

    Added new arrays to ToppingType, one for sound file names and another for the individual count of ingredients sliced

--- src/main/java/edu/pacific/comp55/starter/ToppingType.java
+++ src/main/java/edu/pacific/comp55/starter/ToppingType.java
@@ -2,13 +2,18 @@ package edu.pacific.comp55.starter;
 import java.lang.String;
 
 public enum ToppingType {
-	BACON, CHEESE, EGG, CAN, PINEAPPLE, CLOCK, ROCK;
+	BACON, CHEESE, EGG, //TOMATO, // add toppings on this line
+	CAN, PINEAPPLE, CLOCK, ROCK; //add hazards/updates on this line
 	
 	 public String toString() {
 		switch(this) {
+		//toppings
 			case BACON: return "bacon";
 			case CHEESE: return "cheese";
 			case EGG: return "egg";
+			//case TOMATO: return "tomato";
+			
+		// hazards/updates
 			case CAN: return "can";
 			case PINEAPPLE: return "pineapple";
 			case CLOCK: return "clock";
@@ -16,5 +21,8 @@ public enum ToppingType {
 		}
 		return "n/a";
 	}
+	 
+	 public static String soundFileName[] = new String[] {"slice.mp3", "slice.mp3", "EggCrack.mp3"};
+	 public static int sliceCount[] = new int[] {0,0,0};
 
 }
